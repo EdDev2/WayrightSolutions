7c363a915e96bb2ef11de5da5b4aa7baf74ba0c9
Ed <ed.wenman1@hotmail.com>
2026-08-21 15:38:28 +0100

    Fix privacy page styling

--- src/privacy.jsx
+++ src/privacy.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import PrivacyNotice from './components/PrivacyNotice'
+import './privacy.css'
 
-const root = document.getElementById('root')
-
-createRoot(root).render(
+createRoot(document.getElementById('root')).render(
   <PrivacyNotice />
 )
