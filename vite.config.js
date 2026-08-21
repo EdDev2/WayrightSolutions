3687dce129e81c77ec5563d715e16dd43e1f5f55
Ed <ed.wenman1@hotmail.com>
2026-08-21 15:19:07 +0100

    Fix privacy page build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 export default defineConfig({
- plugins: [react()],
+  plugins: [react()],
+
   base: '/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        privacy: resolve(process.cwd(), 'privacy.html'),
+      },
+    },
+  },
 })
